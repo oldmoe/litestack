954848de174f7b78d9a8fa71cad726a13c1022e9
Brad Gessler <bradgessler@gmail.com>
2023-06-13 12:07:16 -0700

    Add template.rb to make creating litestack apps easier from rails new

---
+++ template.rb
@@ -0,0 +1,7 @@
+# Run `rails new -t https://raw.githubusercontent.com/bradgessler/litestack/master/template.rb`
+# to create a new Rails app with Litestack pre-installed.
+gem "litestack", github: "bradgessler/litestack"
+
+after_bundle do
+  generate "litestack:install"
+end
