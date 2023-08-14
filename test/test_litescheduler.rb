d66c0d1ee16d95da3e25b75f1c14efdae60c39fc
Stephen Margheim <stephen.margheim@gmail.com>
2023-08-14 16:09:52 +0200

    Add some initial tests for Litescheduler

---
+++ test/test_litescheduler.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+describe Litescheduler do
+  describe "#backend" do
+    after do
+      Litescheduler.instance_variable_set(:@backend, nil)
+    end
+    
+    it "when Fiber.scheduler present, returns an instance of the Fiber adapter" do
+      Scheduler = Class.new do # standard:disable Lint/ConstantDefinitionInBlock
+        def block = nil
+      
+        def unblock = nil
+      
+        def kernel_sleep = nil
+      
+        def io_wait = nil
+      end
+      Fiber.set_scheduler Scheduler.new
+      
+      assert_equal :fiber, Litescheduler.backend
+      
+      Fiber.set_scheduler nil
+    end
+
+    it "when Polyphony defined, returns an instance of the Polyphony adapter" do
+      Polyphony = Class.new # standard:disable Lint/ConstantDefinitionInBlock
+      
+      assert_equal :polyphony, Litescheduler.backend
+      
+      Object.send(:remove_const, :Polyphony)
+    end
+
+    it "when nothing is present, returns an instance of the Thread adapter" do
+      assert_equal :threaded, Litescheduler.backend
+    end
+  end
+end
