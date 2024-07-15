d5c712c7d466863f30eb08e197ddbc796664ebcd
Sergey Moiseev <moiseev.sergey@gmail.com>
2024-07-15 18:46:20 +0300

    Dump values in unique list

--- lib/litestack/litekd/unique_list.rb
+++ lib/litestack/litekd/unique_list.rb
@@ -3,16 +3,16 @@ module Litekd
 
     def push(member)
       conn.transaction do
-        conn.delete_composite_member_by_value(@key, member)
-        conn.rpush_composite_member(@key, member)
+        conn.delete_composite_member_by_value(@key, dump(member))
+        conn.rpush_composite_member(@key, dump(member))
         _after_change
       end
     end
     
     def lpush(member)
       conn.transaction do
-        conn.delete_composite_member_by_value(@key, member)
-        conn.lpush_composite_member(@key, member)
+        conn.delete_composite_member_by_value(@key, dump(member))
+        conn.lpush_composite_member(@key, dump(member))
         _after_change
       end
     end
