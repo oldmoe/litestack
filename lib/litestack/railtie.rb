1dd0eb0dc9d350999be9d3e94aaebde8b48f83a8
Asger Behncke Jacobsen <a@asgerbehnckejacobsen.dk>
2024-08-25 00:12:29 +0200

    fix: make it possible to use litestack without active_record

--- lib/litestack/railtie.rb
+++ lib/litestack/railtie.rb
@@ -3,7 +3,7 @@ require "rails/railtie"
 module Litestack
   class Railtie < ::Rails::Railtie
     initializer :disable_production_sqlite_warning do |app|
-      if config.active_record.key?(:sqlite3_production_warning)
+      if config.respond_to?(:active_record) && config.active_record.key?(:sqlite3_production_warning)
         # The whole point of this gem is to use sqlite3 in production.
         app.config.active_record.sqlite3_production_warning = false
       end
